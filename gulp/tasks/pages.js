94f13f146a3f88421390177d2a25abcac7db5bdd
David Baughman <david.baughman@capitalone.com>
2015-08-12 13:25:39 -0700

    Added more code examples, added basic header

--- gulp/tasks/pages.js
+++ gulp/tasks/pages.js
@@ -3,6 +3,7 @@
 var gulp        = require( 'gulp' );
 var paths       = require( '../../gulp/strings' ).paths;
 var nunjucks    = require( 'gulp-nunjucks-html' );
+var connect     = require( 'gulp-connect' );
 
 gulp.task( 'pages', function() {
     return gulp.src( paths.templatesContent )
@@ -16,5 +17,6 @@ gulp.task( 'pages', function() {
             return env;
         }
     }))
-    .pipe( gulp.dest( paths.root ));
+    .pipe( gulp.dest( paths.root ))
+    .pipe( connect.reload() );
 });
