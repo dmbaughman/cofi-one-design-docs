8064a457959b0f417526b2fb087ad18898f73669
David Baughman <david.baughman@capitalone.com>
2015-08-21 14:15:03 -0700

    Updated contents throughout and some styles

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -2,7 +2,7 @@
 
 var gulp = require( 'gulp' );
 
-gulp.task( 'icon-font', function() {
-   gulp.src('./node_modules/bootstrap-sass/node_modules/oneui-icons/fonts/**/*.{ttf,woff,woff2,eot,svg}')
+gulp.task( 'fonts', function() {
+   gulp.src('./node_modules/ui-framework/fonts/**/*.{ttf,woff,woff2,eot,svg}')
    .pipe(gulp.dest('./fonts'));
 });
